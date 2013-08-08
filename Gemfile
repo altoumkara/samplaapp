2f572843d94f069ea2cdbc7c0092732359d2fc87
alama <alama.tounkara@gmail.com>
2013-08-08 05:47:22 -0600

    fixed

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
 source 'https://rubygems.org'
-ruby '2.0.0'
+
 #ruby-gemset=railstutorial_rails_4_0
+
+
+
 
 gem 'rails', '4.0.0'
 
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Samplaapp::Application.config.session_store :cookie_store, key: '_samplaapp_session'
+Samplaapp::Application.config.session_store :cookie_store, key: '_r_session'
